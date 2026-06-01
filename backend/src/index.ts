d822a32865d123a1594be8bb90ea66613c10252b
thebeast01 <mohammadsaif0847@gmail.com>
2026-06-02 02:24:38 +0530

    Updated Cors options

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors({
     // origin: ['https://todo-app-212w.vercel.app'],
-    origin: ["https://todo-app-z7g1.vercel.app", "http://localhost:5173"],
+    origin: ["https://todo-app-z7g1.vercel.app", "https://employee-management-uzdd.vercel.app", "http://localhost:5173"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
