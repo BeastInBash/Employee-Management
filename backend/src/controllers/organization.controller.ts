65997538807f1f7b88051ce3cd9efc2a1e4d8bc6
thebeast01 <mohammadsaif0847@gmail.com>
2026-06-27 14:16:15 +0530

    Add : Get Org by id and get all your org

--- backend/src/controllers/organization.controller.ts
+++ backend/src/controllers/organization.controller.ts
@@ -35,3 +35,42 @@ export const createOrganization = async (req: Request, res: Response) => {
     })
 }
 
+export const getMyOrganization = async (req: Request, res: Response) => {
+    const { userId } = req.user!
+    const allOrgs = await prisma.organization.findMany({
+        where: {
+            ownerId: userId
+        }, include: {
+            memberships: true,
+            workspaces: true
+        }
+    })
+    res.json({
+        status: 200,
+        message: "Your Organizations",
+        allOrgs
+
+    })
+}
+
+export const getOrgById = async (req: Request, res: Response) => {
+    const { orgId, membership, workspace } = req.query
+    const includeMembership = membership == "true"
+    const includeWorkspaces = workspace == "true"
+    const org = await prisma.organization.findFirst({
+        where: {
+            id: orgId as string
+        },
+        include: {
+            memberships: includeMembership,
+            workspaces: includeWorkspaces
+
+        }
+
+    })
+    res.json({
+        status: 200,
+        message: "Fetch Successfull",
+        org
+    })
+}
--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -6,19 +6,15 @@ export function authenticate(
     res: Response,
     next: NextFunction
 ): void {
-    try {
-        const authHeader = req.headers.authorization;
-        const cookieHeader = req.cookies;
-        const bearerToken = authHeader?.startsWith("Bearer ") ? authHeader.slice(7) : undefined
-        const cookiesToken = cookieHeader.token
+    const authHeader = req.headers.authorization;
+    const cookieHeader = req.cookies;
+    const bearerToken = authHeader?.startsWith("Bearer ") ? authHeader.slice(7) : undefined
+    const cookiesToken = cookieHeader.token
 
 
-        const token = cookiesToken;
-        const payload = verifyToken(token);
+    const token = cookiesToken;
+    const payload = verifyToken(token);
 
-        req.user = payload;
-        next();
-    } catch (error) {
-        res.status(401).json({ message: "Invalid or expired token" });
-    }
+    req.user = payload;
+    next();
 }
